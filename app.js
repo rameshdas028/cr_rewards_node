907789fab592953eeeb9ae0ef4ffb88771946141
chandan28Sahoo <chandan19@navgurukul.org>
2022-04-14 10:34:04 +0530

    852

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.options('*', cors());
 app.use(cors());
